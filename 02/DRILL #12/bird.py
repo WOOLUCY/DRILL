547121d4ad74be649f67057b6ffbd445b6006bb4
WOOLUCY <kikanaidek@gmail.com>
2021-11-15 14:16:17 +0900

    Drill #12 완성
    https://youtu.be/YDGt9-Kkbdg

--- 02/DRILL #12/bird.py
+++ 02/DRILL #12/bird.py
@@ -4,8 +4,8 @@ import game_framework
 import game_world
 
 # Bird  Speed
-PIXEL_PER_METER = (10.0 / 0.3) # 10 pixel 30 cm
-RUN_SPEED_KMPH = 30.0 # Km / Hour
+PIXEL_PER_METER = (100.0 / 3) # 100 pixel 300 cm
+RUN_SPEED_KMPH = 70.0 # Km / Hour
 RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
